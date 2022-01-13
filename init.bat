baf760e8fc06b058c38a8c3be30f0139c84e8ed1
Daniel <brikbusters@gmail.com>
2022-01-12 23:35:08 -0500

    windows can't do crap, just install msys2

--- init.bat
+++
@@ -1,5 +0,0 @@
-winget install -e --id GnuWin32.Make
-setx PATH "%PATH%;C:\Program Files (x86)\GnuWin32\bin"
-winget install -e --id Git.Git
-winget install -e --id AltDrag.AltDrag
-winget install -e --id M2Team.NanaZip
