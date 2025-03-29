a134d0b41484de42cbf678d3c9818dc6d1af8f36
Daniel C <brikbusters@gmail.com>
2025-03-28 23:16:15 -0400

    tweak

--- osx.cmake
+++ osx.cmake
@@ -2,7 +2,7 @@
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64/
 set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_VERSION 11)
-set(CMAKE_OSX_DEPLOYMENT_TARGET 10.14)
+set(CMAKE_OSX_DEPLOYMENT_TARGET 10.15)
 
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
@@ -17,13 +17,12 @@ set(CMAKE_C_COMPILER_TARGET x86_64-apple-darwin-macho)
 set(CMAKE_CXX_COMPILER_TARGET x86_64-apple-darwin-macho)
 set(CMAKE_ASM_COMPILER_TARGET x86_64-apple-darwin-macho)
 
-set(CMAKE_SYSROOT /home/daniel/Pulled/MacOSX-SDKs/MacOSX11.3.sdk)
-#set(CMAKE_SYSROOT /home/daniel/Pulled/MacOSX-SDKs/MacOSX10.4u.sdk)
+set(CMAKE_SYSROOT /home/daniel/Pulled/macos-sdk/MacOSX15.2.sdk)
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
-#set(CMAKE_FIND_ROOT_PATH /home/daniel/Pulled/MacOSX-SDKs/MacOSX11.3.sdk)
 
-#set(CMAKE_C_FLAGS_INIT "-fuse-ld=/usr/local/cctools/bin/ld -Wno-unused-command-line-argument")
-#set(CMAKE_CXX_FLAGS_INIT "-fuse-ld=/usr/local/cctools/bin/ld -Wno-unused-command-line-argument")
+set(CMAKE_C_FLAGS_INIT "-target x86_64-apple-darwin-macho")
+set(CMAKE_CXX_FLAGS_INIT "-target x86_64-apple-darwin-macho")
+set(CMAKE_OBJC_FLAGS_INIT "-target x86_64-apple-darwin-macho")
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=/usr/local/cctools/bin/ld")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
