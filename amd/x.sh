1d24afd46342243783827f1534f0b70b6e5c981e
Daniel C <brikbusters@gmail.com>
2025-09-09 14:04:46 -0400

    rework containers, delete old stuff

--- amd/x.sh
+++
@@ -1,3 +0,0 @@
-sudo docker buildx build -t daniel-amd64 .
-
-sudo docker run -v .:/host --device /dev/fuse --privileged --rm -it daniel-amd64 /bin/bash
--- arm/x.sh
+++
@@ -1,7 +0,0 @@
-# setup qemu-user
-sudo docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-
-sudo docker buildx build --platform linux/arm64/v8 -t daniel-arm64 .
-
-# Run container
-sudo docker run -v .:/host --device /dev/fuse --privileged --rm -it daniel-arm64 /bin/bash
